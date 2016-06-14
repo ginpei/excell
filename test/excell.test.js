a2b82d286d454a5b9c0cdd2654b3423aecdd14e7
ginpei <ginpei@gmail.com>
2016-06-13 23:58:09 -0700

    add the first test

--- test/excell.test.js
+++ test/excell.test.js
@@ -1,7 +1,32 @@
 var expect = require('chai').expect;
+var fs = require('fs');
+var jsdom = require('jsdom');
 
-describe('Test', ()=>{
-	it('is working well', ()=>{
-		expect(!false).to.be.true;
+describe('ExCell', ()=>{
+	var ExCell;
+	var excell;
+	var el;
+
+	beforeEach((done)=>{
+		jsdom.env({
+			html: fs.readFileSync('test/excell.test.html', 'utf-8'),
+			src: [
+				fs.readFileSync('./excell.js', 'utf-8'),
+			],
+			done: (err, window)=>{
+				ExCell = window.ExCell;
+				el = window.document.querySelector('table');
+
+				excell = ExCell.create({
+					el: el,
+				});
+
+				done();
+			},
+		});
+	});
+
+	it('adds a class to a target table', ()=>{
+		expect(el.classList.contains('excell-table')).to.be.true;
 	});
 });
